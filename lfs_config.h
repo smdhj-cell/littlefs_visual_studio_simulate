65e6228f6dd765fd9d4887eb72e13060d922602b
smdhj-cell <62281633+smdhj-cell@users.noreply.github.com>
2020-03-20 16:09:36 +0800

    Update lfs_config.h
    修改后体验检验

--- lfs_config.h
+++ lfs_config.h
@@ -6,8 +6,19 @@
 #include <errno.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <string.h>
+#include <assert.h>
 #include "lfs.h"
+#include "lfs_util.h"
 
+typedef struct cfg{
+    uint32_t read_size;
+    uint32_t prog_size;
+    uint32_t block_size;
+    uint32_t block_count;
+} cfg_t;
+
+#define LITTLE_FS_MAX_LOOKAHEAD (32 * ((LITTLE_FS_MAX_BLOCK_COUNT + 31) / 32))
 
 #define LITTLE_FS_MAX_BLOCK_COUNT (3484)
 
@@ -17,14 +28,14 @@
  *        此值过小，每次读取都会与flash进行通讯
  *
  */
-#define LITTLE_FS_READ_SIZE (32)
+#define LITTLE_FS_READ_SIZE (128)
 
 /**
  * @brief 与flash通讯每次至少写入多少字节
  *        必须是LITTLE_FS_READ_SIZE的倍数
  *
  */
-#define LITTLE_FS_PROGRAM_SIZE (32)
+#define LITTLE_FS_PROGRAM_SIZE (128)
 
 /**
  * @brief flash每次至少擦除多少字节
